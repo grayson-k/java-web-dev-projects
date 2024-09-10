328acf1fd18393e00bae10365598134422b4e138
Grayson Keaton <gkeaton23@gmail.com>
2024-09-10 14:24:42 +0200

    completed chapter 2 studio

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/RectangeAreaCalculator.java
@@ -0,0 +1,15 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class RectangeAreaCalculator {
+    public static void main{
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter rectangle length: ");
+        Num length = input.nextLine();
+        System.out.println();
+
+        System.out.println("Enter rectangle width: ");
+        Num width = input.nextLine();
+    }
+}
