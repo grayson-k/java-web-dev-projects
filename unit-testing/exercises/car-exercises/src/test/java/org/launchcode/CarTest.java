922f32aad8134bef4ca566362440651fae8248d4
Grayson Keaton <gkeaton23@gmail.com>
2024-09-19 14:22:47 +0200

    finished exercise

--- unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
@@ -3,6 +3,8 @@ package org.launchcode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.security.PublicKey;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
@@ -26,6 +28,23 @@ class CarTest {
     }
 
     //TODO: gasTankLevel is accurate after driving within tank range
+    @Test
+    public void testGasTankAfterDriving() {
+        test_car.drive(50);
+        assertEquals(9, test_car.getGasTankLevel(), .001);
+    }
+
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    @Test
+    public void gasBeyondRange() {
+        test_car.drive(500);
+        assertFalse(test_car.getGasTankLevel() > 0);
+    }
+
     //TODO: can't have more gas than tank size, expect an exception
+    @Test()
+    public void testGasOverfillException() {
+        assertThrows(IllegalArgumentException.class, () -> test_car.addGas(5), "Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+    }
+
 }
