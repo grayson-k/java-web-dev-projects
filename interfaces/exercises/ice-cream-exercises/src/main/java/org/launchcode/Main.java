de7cbc40da81ba1e20ba60672282e56dc773c6c9
Grayson Keaton <gkeaton23@gmail.com>
2024-09-27 12:13:31 +0200

    completed exercise

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -8,9 +8,12 @@ public class Main {
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
 
+
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
+        flavors.sort(new FlavorComparator());
 
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
+        cones.sort(new ConeComparator());
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
