34c95868d94435b50c4f82ed09cc3fc0363163aa
Grayson Keaton <gkeaton23@gmail.com>
2024-10-01 10:19:52 +0200

    updated studio

--- controllers-and-routing/studio/skills-tracker/src/main/java/org/launchcode/skills_tracker/controller/SkillsController.java
+++ controllers-and-routing/studio/skills-tracker/src/main/java/org/launchcode/skills_tracker/controller/SkillsController.java
@@ -6,57 +6,70 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @ResponseBody
 public class SkillsController {
-
-    @GetMapping("/")
-    public String displaySkills() {
-        return "<html><body>" +
+        @GetMapping("/")
+    @ResponseBody
+    public String hello(){
+        return "<html>" +
+                "<body>" +
                 "<h1>Skills Tracker</h1>" +
-                "<h2>We have a few skills we would like to learn. Here is the list!</h2>" +
+                "<h2>We Learning!</h2>" +
                 "<ol>" +
                 "<li>Java</li>" +
                 "<li>JavaScript</li>" +
                 "<li>Python</li>" +
-                "</ol>" +
-                "</html></body>";
+                "</body>" +
+                "</html>";
     }
 
     @GetMapping("/form")
-    public String displayForm() {
-        return "<form method='post'>" +
-                "<label for='name'>Name:</label><br>" +
-                "<input type='text' id='name' name='name'><br>" +
-                "<label for='firstChoice'>My favorite language:</label><br>" +
-                "<select name='firstChoice' id='firstChoice'>" +
-                "    <option value='Java'>Java</option>" +
-                "    <option value='JavaScript'>JavaScript</option>" +
-                "    <option value='Python'>Python</option>" +
-                "</select><br>" +
-                "<label for='secondChoice'>My second favorite language:</label><br>" +
-                "<select name='secondChoice' id='secondChoice'>" +
-                "    <option value='Java'>Java</option>" +
-                "    <option value='JavaScript'>JavaScript</option>" +
-                "    <option value='Python'>Python</option>" +
-                "</select><br>" +
-                "<label for='thirdChoice'>My third favorite language:</label><br>" +
-                "<select name='thirdChoice' id='thirdChoice'>" +
-                "    <option value='Java'>Java</option>" +
-                "    <option value='JavaScript'>JavaScript</option>" +
-                "    <option value='Python'>Python</option>" +
-                "</select><br>" +
-                "<input type='submit' value='Submit'>" +
-                "</form>";
+    public String form(){
+        String html =
+                "<html>" +
+                        "<body>" +
+                        "<form method = 'post' action = '/hello'>" +
+                        "<label for='name'>Name</label><br>" +
+                        "<input type='text' id='name' name='name'><br>" +
+                        "<label for='firstChoice'>My favorite language:</label><br>" +
+                        "<select name='firstChoice' id='firstChoice'>" +
+                        "<option value='Java'>Java</option>" +
+                        "<option value='JavaScript'>JavaScript</option>" +
+                        "<option value='Python'>Python</option>" +
+                        "</select><br>" +
+                        "<label for='secondChoice'>My second favorite language:</label><br>" +
+                        "<select name='secondChoice' id='secondChoice'>" +
+                        "<option value='Java'>Java</option>" +
+                        "<option value='JavaScript'>JavaScript</option>" +
+                        "<option value='Python'>Python</option>" +
+                        "</select><br>" +
+                        "<label for='thirdChoice'>My third favorite language:</label><br>" +
+                        "<select name='thirdChoice' id='thirdChoice'>" +
+                        "<option value='Java'>Java</option>" +
+                        "<option value='JavaScript'>JavaScript</option>" +
+                        "<option value='Python'>Python</option>" +
+                        "</select><br>" +
+                        "<input type = 'submit' value = 'Rank Me!' />" +
+                        "</form>" +
+                        "</body>" +
+                        "</html>";
+        return html;
     }
-
-    @RequestMapping(value = "form", method = RequestMethod.POST)
-    public String processForm(@RequestParam String name,
-                              String firstChoice,
-                              String secondChoice,
-                              String thirdChoice) {
-        return "<h1>" + name + "</h1>" +
+    //    @GetMapping("/hello")
+    @RequestMapping(value="hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name, String firstChoice, String secondChoice, String thirdChoice ) {
+        return "<html>" +
+                "<body>" +
+                "<h1>" + name + "</h1>" +
                 "<ol>" +
-                "<li>" + firstChoice + "</li>" +
-                "<li>" + secondChoice + "</li>" +
-                "<li>" + thirdChoice + "</li>" +
-                "</ol>";
+                "<li>" +
+                firstChoice +
+                "</li>" +
+                "<li>" +
+                secondChoice +
+                "</li>" +
+                "<li>" +
+                thirdChoice +
+                "</li>" +
+                "</body>" +
+                "</html>";
     }
 }
